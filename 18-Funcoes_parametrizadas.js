d7aead6e803847c906dfae267df5f9125a7a5125
Michele Lozada <michelelozada1@gmail.com>
2022-04-20 17:34:02 -0300

    Edited file

--- 18-Funcoes_parametrizadas.js
+++ 18-Funcoes_parametrizadas.js
@@ -1,5 +1,5 @@
 /* Dado o seguinte array de números [1,2,3,4,5,6,7,8,9,10] e um número (que vamos considerar aqui como sendo 7),
- escreva um algoritmo que crie uma nova lista para armazenar *apenas* os valores menores que 'n' */
+ escreva um algoritmo que crie uma nova lista para armazenar *apenas* os valores menores que 'num' */
 
 // Criando a função criarNovoArray
 function criarNovoArray(originalArray, num){
